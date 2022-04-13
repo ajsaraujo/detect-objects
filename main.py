5d44ad447b6064aee55ff47beb825b9251f70860
Allan Juan <allanjuansil@gmail.com>
2022-04-13 18:34:08 -0300

    Adicionar implementação do algoritmo

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def read_pgm(file_path):
   file = open(file_path, "r")
   file_type = read_line(file)
 
-  if file_type != 'P2':
-    error(f'Expected file type to be P2 (grayscale), got "{file_type}" instead')
+  if file_type != 'P1':
+    error(f'Expected file type to be P1 (binary), got "{file_type}" instead')
 
   width, height = map(int, read_line(file).split())
   max_value = read_int(file)
@@ -52,6 +52,36 @@ def read_pgm(file_path):
     'matrix': matrix
   }
 
+def get_neighbors(i, j):
+  deltas = [-1, 0, 1]
+
+  neighbors = []
+
+  for delta_x in deltas:
+    for delta_y in deltas:
+      neighbor_x = i + delta_x
+      neighbor_y = j + delta_y
+
+      if neighbor_x < 0 or neighbor_x >= width:
+        continue
+      
+      if neighbor_y < 0 or neighbor_y >= height:
+        continue
+      
+      neighbors.append([neighbor_x, neighbor_y])
+  
+  return neighbors
+
+def fill(i, j):
+  for neighbor in get_neighbors(i, j):
+    m, n = neighbor
+
+    not_visited = tags[m][n] == NO_TAG
+    same_color_as_current_pixel = color[i][j] == color[m][n]
+
+    if not_visited and same_color_as_current_pixel:
+      fill(m, n)
+
 file_path_was_passed = len(sys.argv) > 1
 
 if not file_path_was_passed:
@@ -59,3 +89,22 @@ if not file_path_was_passed:
 
 file_path = sys.argv[1]
 pgm = read_pgm(file_path)
+tags = zeroes_matrix(pgm['height'], pgm['width'])
+color = pgm['matrix']
+
+NO_TAG = 0
+last_tag = 0
+height = pgm['height']
+width = pgm['width']
+
+for i in range(height):
+  for j in range(width):
+    if tags[i][j] == NO_TAG:
+      last_tag += 1
+      fill(i, j)
+
+      if color[i][j] == 0:
+        print(f'Found blank space at ({i}, {j})')
+      else:
+        print(f'Found new object at ({i}, {j}')
+      
